f41f203db9000c524629aa4d578b07c5e519502e
AliFletcher <aliasghar78gaming@gmail.com>
2019-11-07 15:34:35 +0330

    sign up form

--- navbar/views.py
+++ navbar/views.py
@@ -9,4 +9,11 @@ def first_page(request):
 
 def sign_up_page(request):
     form = forms.SignUpForm
+
+    if request.method == "POST":
+        form = forms.SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "home_page.html")
+
     return render(request, "sign_up.html", {'form': form})
