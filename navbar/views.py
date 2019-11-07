34817c320dfabfe7202200d84b20a22b85676613
AliFletcher <aliasghar78gaming@gmail.com>
2019-11-07 14:25:19 +0330

    made the navbar and homepage

--- navbar/views.py
+++ navbar/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def first_page(request):
-    return render(request, "first_page.html")
+    return render(request, "home_page.html")
