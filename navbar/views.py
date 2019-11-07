91ff0be17a4af56b9c076366e8661fc68a70adfd
AliFletcher <aliasghar78gaming@gmail.com>
2019-11-07 17:30:57 +0330

    hoooraaaaa

--- navbar/views.py
+++ navbar/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from navbar import forms
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -17,3 +18,14 @@ def sign_up_page(request):
             return first_page(request)
 
     return render(request, "sign_up.html", {'form': form})
+
+
+def my_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+    else:
+        return render(request, "login.html", {'message': 'invalid password or username'})
