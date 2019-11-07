ceca18114312c44d4f774da2482d8fdeb4cc12f8
AliFletcher <aliasghar78gaming@gmail.com>
2019-11-07 15:25:48 +0330

    :)))

--- navbar/models.py
+++ navbar/models.py
@@ -7,7 +7,7 @@ from django import forms
 class User(models.Model):
     first_name = models.CharField(max_length=264)
     last_name = models.CharField(max_length=264)
-    user_name = models.CharField(max_length=264)
+    username = models.CharField(max_length=264)
     email = models.EmailField()
-    password = models.CharField(max_length=264)
-    verify_password = models.CharField(max_length=264)
+    password1 = models.CharField(max_length=264)
+    password2 = models.CharField(max_length=264)
